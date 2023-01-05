9773fb84e2827c087c72043020cd5d6bf7f73ae8
Jerson Fuentes <47575457+jersonfuentes@users.noreply.github.com>
2023-01-05 03:59:25 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World</p>'; ?> 
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
