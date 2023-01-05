8d907345d7f10b1e5b61ea0f9ac2bbd063e9625d
Jerson Fuentes <47575457+jersonfuentes@users.noreply.github.com>
2023-01-05 04:12:53 +0000

    Ejemplo inicial

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php 
+    echo '<p>Hello World</p>'; 
+    
+    function suma(){
+      $valor1 = 8; 
+      $valor2 = 6;
+      $resultado = $valor1 + $valor2;
+      print_r($resultado);
+    }
+     suma();
+    ?> 
     <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
   </body>
 </html>
